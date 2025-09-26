e0e0ee29b22d6fa9b07ae5b0a6565b940ea23d6e
Dawid Kurzyniec <dawid.kurzyniec@gmail.com>
2025-09-26 14:25:41 +0200

    Including all the headers, because the order of inclusion could otherwise be quite confusing, and the compilation cost of including all is likely to be low.

--- src/roo_quantity.h
+++ src/roo_quantity.h
@@ -1,3 +1,31 @@
 #pragma once
 
+// Base.
+
+#include "roo_quantity/frequency.h"
+#include "roo_quantity/time.h"
 #include "roo_quantity/temperature.h"
+
+// Mechanics.
+
+#include "roo_quantity/mass.h"
+#include "roo_quantity/area.h"
+#include "roo_quantity/areic_number.h"
+#include "roo_quantity/force.h"
+#include "roo_quantity/length.h"
+#include "roo_quantity/lineic_number.h"
+#include "roo_quantity/power.h"
+#include "roo_quantity/pressure.h"
+#include "roo_quantity/velocity.h"
+#include "roo_quantity/volume.h"
+#include "roo_quantity/volumic_number.h"
+#include "roo_quantity/volume_flow_rate.h"
+#include "roo_quantity/work.h"
+
+// Electricity.
+
+#include "roo_quantity/capacitance.h"
+#include "roo_quantity/charge.h"
+#include "roo_quantity/current.h"
+#include "roo_quantity/resistance.h"
+#include "roo_quantity/voltage.h"
